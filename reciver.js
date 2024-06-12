5b1f175a7c69a60e2318af3df9d8fe077a509d77
zmxthiasXD༒ <140333241+zmxthiasMC@users.noreply.github.com>
2024-06-11 22:53:58 -0400

    reciver.js

---
+++ reciver.js
@@ -0,0 +1,24 @@
+const dgram = require('dgram');
+const client = dgram.createSocket('udp4');
+
+const ipList = []
+let messages = 0;
+
+client.on('listening', () => {
+  console.log(`[RECEIVER] Listening on port ${process.argv[2] || 8080}...`)
+});
+
+client.on('message', (msg, rinfo) => {
+  if (!ipList.includes(rinfo.address)) {
+    ipList.push(rinfo.address)
+    console.log(`[RECEIVER] New IP: ${rinfo.address}`)
+  }
+  messages++;
+})
+
+setInterval(() => {
+  console.log(`[RECEIVER] ${messages} packets por segundo`)
+  messages = 0;
+}, 1000)
+
+client.bind(process.argv[2] || 8080);
